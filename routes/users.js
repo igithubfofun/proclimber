f989b8da00b16897fdafa1b1c6ae43a966142688
Metel Patel <metelpatel@gmail.com>
2016-09-20 20:21:30 -0500

    user undefined

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,21 @@ var User = require('../models/user');
 
 /* GET users listing. */
 router.get('/all', function(req, res, next) {
-  User.find({}, function(err, results) {
+  User.findOne({_id: '57e1c29c48a6352523b31659'}, function(err, results) {
     if (err) console.log(err);
+    res.render('all', {name: user.name});
   });
-  res.render('all', {user: user.name});
+});
+
+router.get('/', function(req, res) {
+  User.find({}, function(err, results) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    console.log(User._id);
+    res.status(200).json(results);
+  });
 });
 
 router.get('/signup', function(req, res) {
